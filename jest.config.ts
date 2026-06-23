6dee2bdf4e92f1767473b493719f7afa583cd856
Herrera, Bruno Javier <brunoherrera1158@gmail.com>
2026-06-23 17:49:11 -0300

    :zap: Configure CI pipeline

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,17 @@
-import nextJest from 'next/jest.js'
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   // Le indica a Next.js dónde está la raíz del proyecto para cargar .env y next.config
   dir: './',
-})
+});
 
 // Configuración personalizada para Jest
 const config = {
   // Simula un entorno de navegador por si luego quieres testear componentes de React
-  testEnvironment: 'jest-environment-jsdom',
-}
+  // testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
 
 // Exportar la configuración para que Jest la use
-export default createJestConfig(config)
+export default createJestConfig(config);
