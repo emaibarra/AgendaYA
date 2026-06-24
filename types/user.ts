6109159301e1d54d3037007599d6ad0b8ca5d7ed
Herrera, Bruno Javier <brunoherrera1158@gmail.com>
2026-06-23 22:10:12 -0300

    :bug: fix en el formato

--- types/user.ts
+++ types/user.ts
@@ -1,12 +1,12 @@
 export interface RegisterDto {
-    name: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-  }
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
-  export interface User {
-    name: string;
-    email: string;
-    password: string;
-  }
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+}
